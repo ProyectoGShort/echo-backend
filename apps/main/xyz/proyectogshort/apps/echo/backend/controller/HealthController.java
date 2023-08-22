8a2f08f17d3be0f50ec3e63ee17abf13de8f75ea
Jesús de la Casa Palomino <jesusdelacasapalomino@gmail.com>
2023-08-22 20:40:36 +0200

    Add health check endpoint

---
+++ apps/main/xyz/proyectogshort/apps/echo/backend/controller/HealthController.java
@@ -0,0 +1,19 @@
+package xyz.proyectogshort.apps.echo.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController()
+public class HealthController {
+
+    @GetMapping("/health")
+    public HashMap<String, String> index() {
+        HashMap<String, String> status = new HashMap<>();
+        status.put("application", "echo_backend");
+        status.put("status", "ok");
+
+        return status;
+    }
+}
