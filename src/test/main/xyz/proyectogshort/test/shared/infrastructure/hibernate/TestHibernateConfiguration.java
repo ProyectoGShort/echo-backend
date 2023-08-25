7c740cf564fb88fa480aec240c086ff7e491f224
Jesús de la Casa Palomino <jesusdelacasapalomino@gmail.com>
2023-08-25 19:19:11 +0200

    Add test database

--- src/test/main/xyz/proyectogshort/test/shared/infrastructure/hibernate/TestHibernateConfiguration.java
+++ src/test/main/xyz/proyectogshort/test/shared/infrastructure/hibernate/TestHibernateConfiguration.java
@@ -24,7 +24,7 @@ public class TestHibernateConfiguration {
     @Bean("testDataSource")
     public DataSource testDataSource() {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/test");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5433/test");
         config.setUsername("postgres");
         config.setPassword("postgres");
         config.setDriverClassName("org.postgresql.Driver");
