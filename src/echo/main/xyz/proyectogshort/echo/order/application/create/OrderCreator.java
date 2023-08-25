3f5c2674bd548bae6e3584001933d3dd41497acf
Jesús de la Casa Palomino <jesusdelacasapalomino@gmail.com>
2023-08-25 19:07:01 +0200

    Add orderEntity creation

--- src/echo/main/xyz/proyectogshort/echo/order/application/create/OrderCreator.java
+++ src/echo/main/xyz/proyectogshort/echo/order/application/create/OrderCreator.java
@@ -1,11 +1,22 @@
 package xyz.proyectogshort.echo.order.application.create;
 
 import xyz.proyectogshort.echo.order.domain.OrderId;
+import xyz.proyectogshort.echo.order.infrastructure.hibernate.OrderEntity;
+import xyz.proyectogshort.echo.order.infrastructure.hibernate.OrderEntityRepository;
 import xyz.proyectogshort.shared.domain.Service;
+
+import java.util.UUID;
 
 @Service
 public final class OrderCreator {
+    private final OrderEntityRepository orderEntityRepository;
+
+    public OrderCreator(OrderEntityRepository orderEntityRepository) {
+        this.orderEntityRepository = orderEntityRepository;
+    }
+
     public void create(OrderId orderId) {
         System.out.println("Order received: " + orderId);
+        orderEntityRepository.save(new OrderEntity(UUID.randomUUID().toString()));
     }
 }
