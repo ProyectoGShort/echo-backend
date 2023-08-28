8bc5fb7c5c73ffb41d3f28b287f858731aaf3f68
Jesús de la Casa Palomino <jesusdelacasapalomino@gmail.com>
2023-08-28 18:18:25 +0200

    Test EventTransactionMiddleware

---
+++ src/test/main/xyz/proyectogshort/test/test/infrastructure/hibernate/application/create/CreateMediaOnOrderCreated.java
@@ -0,0 +1,26 @@
+package xyz.proyectogshort.test.test.infrastructure.hibernate.application.create;
+
+import xyz.proyectogshort.echo.order.domain.OrderCreatedEvent;
+import xyz.proyectogshort.shared.domain.Service;
+import xyz.proyectogshort.shared.domain.bus.event.EventListener;
+import xyz.proyectogshort.test.test.infrastructure.hibernate.TestEntity;
+import xyz.proyectogshort.test.test.infrastructure.hibernate.TestEntityRepository;
+
+import java.util.UUID;
+
+@Service
+public class CreateMediaOnOrderCreated {
+
+    private final TestEntityRepository testEntityRepository;
+
+    public CreateMediaOnOrderCreated(TestEntityRepository testEntityRepository) {
+        this.testEntityRepository = testEntityRepository;
+    }
+
+    @EventListener()
+    public void on(OrderCreatedEvent event) {
+        testEntityRepository.save(new TestEntity(UUID.randomUUID().toString()));
+//        throw new RuntimeException();
+    }
+
+}
